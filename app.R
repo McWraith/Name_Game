37f44f1b117adee063c452cfd09a994d1901f4ce
McWraith <mprzydacz@gmail.com>
2020-10-24 21:37:25 +0100

    Addition of app file

---
+++ app.R
@@ -0,0 +1,12 @@
+##### Project Info #####
+# Author: Michal Przydacz
+
+##### Project Setup #####
+# Load libraries
+library(tidyverse)
+library(googledrive)
+library(tidylog)
+library(shiny)
+library(shinydashboard)
+library(shinycssloaders)
+library(rtweet)
