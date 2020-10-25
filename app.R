d15bc6650ee351c80e6987e0cfc8e3cefdbcbccc
McWraith <mprzydacz@gmail.com>
2020-10-25 20:23:43 +0000

    Remove unnecessary packages fleshout dashboard

--- app.R
+++ app.R
@@ -4,13 +4,18 @@
 # Author: Michal Przydacz
 
 ##### Project Setup #####
+
 # Load libraries
-library(tidyverse)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(readr)
 library(googledrive)
 library(tidylog)
 library(shiny)
 library(shinydashboard)
 library(shinycssloaders)
+library(DT)
 library(rtweet)
 
 ##### Shiny Ui #####
@@ -18,24 +23,61 @@ library(rtweet)
 ui <- dashboardPage(
   dashboardHeader(title = "Name Game Dashboard",
                   titleWidth = "300px"),
-  dashboardSidebar(),
-  dashboardBody()
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Game setup", tabName = "tab_teams",
+               sliderInput("teams_slider", "Number of teams",
+                           min = 1, max = 4, value = 2)),
+      menuItem("Game", tabName = "tab_game")
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "tab_teams",
+              fluidPage(
+                fluidRow(
+                  column(width = 4, 
+                         DTOutput("players_dt"),
+                         actionButton("teams_button", "From teams"))
+                )
+              )
+      ),
+      tabItem(tabName = "tab_game")
+    )
+  )
 )
 
 
 ##### Shiny Server #####
 
 server <- function(input, output, session) {
-  ##### Twitter API setup
-  source("secret")
   
-  token <- rtweet::create_token(app = app, 
-                                consumer_key = consumer_key, 
-                                consumer_secret = consumer_secret,
-                                access_token = access_token,
-                                access_secret = access_secret,
-                                set_renv = FALSE) # Needs to be set to TRUE on first run
+  ## Tab: Game setup -----------------------------------------------------------
   
+  # Import Players
+  players <- reactive({
+    tmp_path <- fs::file_temp(ext = "csv")
+    drive_download("Name_Game/players", path = tmp_path)
+    players <- read_csv(tmp_path, col_types = "c")
+    fs::file_delete(tmp_path)
+    players
+  })
+  
+  output$players_dt <- renderDT({
+    
+    datatable(players(),
+              selection = "none",
+              options = list(
+                dom = "t"
+              )
+    )
+  })
+  
+  # Create random groups
+  player_groups <- eventReactive(input$teams_button, {
+    players_df <- players()
+    
+  })
   
 }
 
